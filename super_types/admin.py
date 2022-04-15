99d0a570c347038979daaea29e4239c05fc7eb04
elijahrosett <rosett3@gmail.com>
2022-04-15 10:47:12 -0600

    Created super class model + created url paths for supers

---
+++ super_types/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+
+from super_types.models import Super_Types
+from .models import Super_Types
+# Register your models here.
+admin.site.register(Super_Types)
---
+++ super_types/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+from .models import Super_Types
+
+
+class Super_Types_Serializer(serializers.ModelSerializer):
+    class Meta:
+        model = Super_Types
+        fields = ['Hero', 'Villian']
+        
