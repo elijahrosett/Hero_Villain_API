a24cb31d1fb23f4d15504c96ee31bfec112090bf
elijahrosett <rosett3@gmail.com>
2022-04-15 13:29:46 -0600

    Created data for supers table + created views for supers

--- heros_villains_project/settings.py
+++ heros_villains_project/settings.py
@@ -42,7 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'super_types'
+    'super_types',
+    'supers'
     
     
 ]
--- supers/views.py
+++ supers/views.py
@@ -3,8 +3,19 @@ from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
-@api_view(['GET'])
-def supers_list(request):
-   
-    return Response('ok')
+from supers import serializers
+from .models import Super
+
+from django.shortcuts import get_object_or_404, get_list_or_404
+
+
+@api_view(['GET', 'POST'])
+def supers_specific(request, pk):
+    super = get_object_or_404(Super, pk=pk)
+    if request.method == 'GET':
+        serializer = serializers.SuperSerializer(super)
+        #super = Super.objects.filter()
+    
+    
+    return Response(serializer.data)
 
